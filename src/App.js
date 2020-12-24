15d8afe86115e52ecfcd92a967d7ea9ed5d403d2
alejandro <alejandrodanielaquino@yahoo.com.ar>
2020-12-24 20:40:21 -0300

    add state: songs, setSongs, currentSong, setCurrentSongs in App. Add styles to h2, h3, h1 and replaced placeholder in song

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import Song from "./components/Song";
 import Player from "./components/Player";
 import "./styles/app.scss";
+import data from "./data";
 
 function App() {
+
+  const [songs, setSongs] = useState(data);
+  const [currentSong, setCurrentSong] = useState(songs[0])
+
+  
   return (
     <div>
-      <Song />
+      <Song songs={songs} setSongs={setSongs} currentSong={currentSong} setCurrentSong={setCurrentSong} />
       <Player />
     </div>
   );
