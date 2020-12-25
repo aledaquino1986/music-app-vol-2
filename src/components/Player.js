0fce0b7be26301c1867d9c635142e1830de9a8b9
alejandro <alejandrodanielaquino@yahoo.com.ar>
2020-12-25 17:08:57 -0300

    update UI duration and current time

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -8,7 +8,14 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function Player({ currentSong, isPlaying, setIsPlaying }) {
+  //Hooks
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0
+  });
   const audioRef = useRef(null);
+
+  // event Handlers
   const playSongHandler = () => {
     if (!isPlaying) {
       audioRef.current.play();
@@ -18,12 +25,29 @@ function Player({ currentSong, isPlaying, setIsPlaying }) {
 
     setIsPlaying(!isPlaying);
   };
+
+  const timeUpdateHandler = e => {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+
+    setSongInfo({
+      currentTime: getTime(currentTime),
+      duration: getTime(duration)
+    });
+  };
+
+  function getTime(time) {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  }
+
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start Time</p>
+        <p>{songInfo.currentTime}</p>
         <input type="range" />
-        <p>End time</p>
+        <p>{songInfo.duration}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon size="2x" className="skip-back" icon={faAngleLeft} />
@@ -40,7 +64,12 @@ function Player({ currentSong, isPlaying, setIsPlaying }) {
         />
       </div>
 
-      <audio ref={audioRef} src={currentSong.audio}></audio>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onLoadedMetadata={timeUpdateHandler}
+      ></audio>
     </div>
   );
 }
